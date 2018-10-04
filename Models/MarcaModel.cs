61f5c1f1c67e7b61a3f12a25605697336329c560
Hemerson Lima <hemerson.lima@assistsoftware.com.br>
2018-10-04 18:26:19 -0300

    Models Development {MarcaModel, ModeloModel}

---
+++ Models/MarcaModel.cs
@@ -0,0 +1,9 @@
+namespace hnl.veiculos.com.br.Models
+{
+    public class MarcaModel
+    {
+        public decimal ID {get; private set; }
+        public string IMAGEM { get; private set; }
+        public string NOME {get; private set;}
+    }
+}
---
+++ Models/ModeloModel.cs
@@ -0,0 +1,11 @@
+namespace hnl.veiculos.com.br.Models
+{
+    public class ModeloModel
+    {
+        public decimal ID { get; private set; }
+        public string NOME { get; private set; }
+        public string IMAGEM { get; private set; }
+        public string MARCA { get; private set; }
+        public decimal IDMARCA { get; private set; }
+    }
+}
